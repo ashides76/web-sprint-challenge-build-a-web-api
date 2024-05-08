3eb6ab5071af1e704978dc654e75fa0d5dffca49
Ashish Desai <desai.ashish76@gmail.com>
2024-05-07 22:46:57 -0400

    wip

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -8,6 +8,39 @@ function projectLogger(req, res, next) {
   next()
 }
 
+// async function validateProjectId (req, res, next) {
+//     const { id } = req.params
+//     await Users.getById(id)
+//       .then(user => {
+//         if (user) {
+//           req.user = user
+//           next()
+//         } else {
+//           next({
+//             status: 404,
+//             message: "user not found"
+//           })
+//         }
+//       })
+//       .catch(err => {
+//         next(err)
+//       })
+  
+//   }
+  
+//   async function validateUser(req, res, next) {
+//     const { name } = req.body
+//     if (!name || !name.trim()) {
+//       res.status(400).json({
+//         message: "missing required name field"
+//       })
+//     } else {
+//       req.name = name.trim()
+//       next()
+//     }
+//   }
+  
+
 module.exports = {
     projectLogger
 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -4,9 +4,37 @@ const Project = require('./projects-model')
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+    await Project.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(next)
+})
+
+router.get('/:id', async(req, res) => {
+
+})
+
+router.post('/', async (req, res) => {
+
+})
+
+router.put('/:id', async(req, res) => {
+
+})
+
+router.delete('/:id', async(req, res) => {
+
+})
+
+router.get('/:id/actions', async(req, res) => {
+    
+})
+
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(err.status || 500).json({
-      customMessage: 'Something bad happend inside the hubs router',
+      customMessage: 'Something bad happend inside the projects router',
       message: err.message,
     });
   });
