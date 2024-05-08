9daa0d2425fc564d5bfc9bc1431cec38b41f865d
Ashish Desai <desai.ashish76@gmail.com>
2024-05-08 10:55:27 -0400

    wip

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -5,11 +5,33 @@ const Action = require('./actions-model')
 
 const router = express.Router()
 
+router.get('/', async (req, res, next) => {
+  await Action.get()
+    .then(action => {
+      res.json(action)
+    })
+    .catch(next)
+})
 
+router.get('/:id', async (req, res) => {
+
+})
+
+router.post('/', async(req, res) => {
+
+})
+
+router.put('/:id', async (req, res) => {
+
+})
+
+router.delete('./id', async (req, res) => {
+
+})
 
 router.use((err, req, res, next) => { //eslint-disable-line
     res.status(err.status || 500).json({
-      customMessage: 'Something bad happend inside the hubs router',
+      customMessage: 'Something bad happend inside the actions router',
       message: err.message,
     });
   });
